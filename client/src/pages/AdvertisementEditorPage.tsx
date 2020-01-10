22b5502c4e95bc9b7c8697872e5d407c2c0e2302
Mac OS <macos@Macs-MacBook-Pro.local>
2020-01-10 22:11:09 +0900

    Advertisement editor title updated

--- client/src/pages/AdvertisementEditorPage.tsx
+++ client/src/pages/AdvertisementEditorPage.tsx
@@ -4,14 +4,17 @@ import CheckLogged from "src/containers/etc/CheckLogged"
 import EditorLayout from "src/components/layout/EditorLayout"
 import Title from "src/components/elements/Title"
 import AdvertisementForm from "src/containers/advertisement/AdvertisementForm"
+import { useParams } from 'react-router-dom'
 
 const AdvertisementEditorPage: React.SFC = () => {
+    const { adId } = useParams()
+
     return (
         <>
             <Header />
             <CheckLogged>
                 <EditorLayout>
-                    <Title>광고 등록하기</Title>
+                    <Title>광고 {adId ? "수정하기" : "등록하기"}</Title>
                     <AdvertisementForm />
                 </EditorLayout>
             </CheckLogged>
